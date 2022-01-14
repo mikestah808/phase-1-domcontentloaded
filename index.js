0fd835d2715838faabe5987bf9b3edeb4f314363
Michael Stafford <michaelstafford808@gmail.com>
2022-01-14 11:47:44 -0700

    commit tree 7089bb4b4b55046b1cdff37cb10ca79c76da9c74

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log("After DOM Loaded");
+    let p = document.querySelector("p")
+    p.textContent = "This is really cool!"
+    console.log(p)
+    
+})
+
+// console.log("Before DOM Loads");
